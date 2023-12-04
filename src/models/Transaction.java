107b1e64a68ad685890dd252331797bf0f5f68b9
Fandresena Ravohary <hei.fandresena@gmail.com>
2023-12-04 13:40:14 +0100

    Feat: add the Transaction model

---
+++ src/models/Transaction.java
@@ -0,0 +1,70 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private String transactionId;
+    private LocalDate date;
+    private double amount;
+    private String type;
+    private String description;
+
+    public Transaction(String transactionId, LocalDate date, double amount, String type, String description) {
+        this.transactionId = transactionId;
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+        this.description = description;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId='" + transactionId + '\'' +
+                ", date=" + date +
+                ", amount=" + amount +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
