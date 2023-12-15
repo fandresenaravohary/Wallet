7c4db8108b68b077a497559fc7b37bf512619fb4
EspereJonathan <hei.espere@gmail.com>
2023-12-15 17:00:25 +0300

    feat: deleted function get_transaction_sum

--- src/scripts/create_table.Transaction.sql
+++ src/scripts/create_table.Transaction.sql
@@ -5,25 +5,3 @@ CREATE TABLE IF NOT EXISTS transaction (
     type VARCHAR(10) NOT NULL,
     label VARCHAR(255) NOT NULL
 );
-
-CREATE OR REPLACE FUNCTION get_transaction_sum(
-    account_id INT,
-    start_datetime TIMESTAMP,
-    end_datetime TIMESTAMP
-)
-RETURNS DOUBLE PRECISION
-AS $$
-DECLARE
-    total_amount DOUBLE PRECISION;
-BEGIN
-    SELECT 
-        COALESCE(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END), 0)
-        - COALESCE(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END), 0)
-    INTO total_amount
-    FROM transaction t
-    WHERE t.account_id = account_id
-    AND t.transaction_date BETWEEN start_datetime AND end_datetime;
-
-    RETURN total_amount;
-END;
-$$ LANGUAGE plpgsql;
